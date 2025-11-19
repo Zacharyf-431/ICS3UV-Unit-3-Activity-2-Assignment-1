377fea53564249bb524886a66d8d966fc8ab4049
Zachary Fowler <zachary.fowler1@stu.ocsb.ca>
2025-11-19 22:38:41 +0000

    Fixed

--- main.ts
+++ main.ts
@@ -6,10 +6,10 @@
  */
 
 // assign constants 
-const num1 = 8.5467;
-const num2 = 9.6382;
-const num3 = 18.5146;
-const num4 = 125.496;
+const num1: number = 8.5467;
+const num2: number = 9.6382;
+const num3: number = 18.5146;
+const num4: number = 125.496;
 
 // INPUT - No input
 
@@ -19,3 +19,5 @@ console.log(`${num1.toFixed(3).padStart(10)}`);
 console.log(`${num2.toFixed(5).padStart(8)}`);
 console.log(`${num3.toFixed(1).padStart(6)}`);
 console.log(`${num4.toFixed(1).padStart(3)}`);
+
+console.log("\nDone.")
